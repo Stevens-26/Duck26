264d4fadb2e2905fd13120486244dc20582ee0bf
BDXGaming <74644043+BDXGaming@users.noreply.github.com>
2022-02-19 17:50:57 -0500

    feat: reformat main file

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,16 +9,18 @@ def load_config_data():
     return toml.load("duck_bot_config.toml")
 
 
-# Loads the config and gets all needed values
-config = load_config_data()
-cogs = config.get("DuckBot").get("cogs")
-token = config.get("DuckBot").get("token")
-prefix = config.get("DuckBot").get("prefix")
+if __name__ == "__main__":
+    # Loads the config and gets all needed values
+    config = load_config_data()
+    cogs = config.get("DuckBot").get("cogs")
+    token = config.get("DuckBot").get("token")
+    prefix = config.get("DuckBot").get("prefix")
 
-# Assigns the discord intents needed for the bot
-intents = discord.Intents.default()
-intents.members = True
+    # Assigns the discord intents needed for the bot
+    intents = discord.Intents.default()
+    intents.members = True
 
-# Creates and runs the instance of the DuckBot class
-bot = DuckBot(command_prefix=prefix, intents=intents, available_cogs=cogs, bot_config=config)
-bot.run(token)
+    # Creates and runs the instance of the DuckBot class
+    bot = DuckBot(command_prefix=prefix, intents=intents, available_cogs=cogs, bot_config=config)
+    bot.run(token)
+
